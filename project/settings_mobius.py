429fa12a2fb1e8480d0adbbf5a5ec500d31c0cab
Johan Beyers <johan@juizi.com>
2016-11-10 09:49:49 +0200

    Add composer to installed apps

--- project/settings_mobius.py
+++ project/settings_mobius.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = (
     "ultracache",
     "sites_groups",
     "mote",
+    "composer",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",

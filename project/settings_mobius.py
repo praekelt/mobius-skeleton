d923476eef8ee8e5b274fb7ec517982af3bdacfa
Johan Beyers <johan@juizi.com>
2016-11-14 10:48:13 +0200

    Actually add context processor

--- project/settings_mobius.py
+++ project/settings_mobius.py
@@ -69,6 +69,7 @@ TEMPLATE_CONTEXT_PROCESSORS = [
     "django.core.context_processors.tz",
     "django.core.context_processors.request",
     "django.contrib.messages.context_processors.messages",
+    "composer.context_processors.slots",
 ]
 
 TEMPLATES = [

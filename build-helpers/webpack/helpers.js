5b039edc648674be645b165084624d275ab1ca47
Chris Sergienko <chris@praekelt.com>
2016-11-15 16:47:19 +0200

    Setup full webpack build and mote dir structure.

---
+++ build-helpers/webpack/helpers.js
@@ -0,0 +1,181 @@
+const
+    Webpack = require('webpack'),
+    CleanWebpackPlugin = require('clean-webpack-plugin'),
+    ExtractTextPlugin = require('extract-text-webpack-plugin'),
+    BundleTracker = require('webpack-bundle-tracker'),
+    StyleLint = require('stylelint-webpack-plugin');
+
+exports.clean = function(path) {
+    return {
+        plugins: [
+            new CleanWebpackPlugin([path], {
+                root: process.cwd() //point to project
+            })
+        ]
+    }
+}
+
+exports.globSass = function() {
+    return {
+        module: {
+            preLoaders: [{
+                test: /\.s[c|a]ss$/,
+                loader: 'import-glob-loader'
+            }]
+        }
+    }
+}
+
+exports.extractCSS = function(opts) {
+    console.log(opts.include[0]);
+
+    return {
+        plugins: [
+            // Output extracted CSS to a file
+            new ExtractTextPlugin(opts.filename)
+        ],
+        module: {
+            loaders: [
+                {
+                    test: /\.s[c|a]ss$/,
+                    loader: ExtractTextPlugin.extract('style', 'css!postcss!sass'),
+                    include: opts.include
+                }
+            ]
+        }
+    }
+}
+
+exports.setupCSS = function(opts) {
+    return {
+        module: {
+            loaders: [
+                {
+                    test: /\.s[c|a]ss$/,
+                    loader: 'style!css!postcss!sass',
+                    include: opts.include
+                }
+            ]
+        }
+    }
+}
+
+exports.lintCSS = function(opts) {
+    return {
+        plugins: [
+            new StyleLint({
+                configFile: opts.configFile,
+                files: opts.fileGlob,
+                failOnError: true,
+                syntax: 'scss'
+            })
+        ]
+    }
+}
+
+exports.setupJS = function(paths) {
+    return {
+        module: {
+            loaders: [
+                {
+                    test: /\.js$/,
+                    include: paths,
+                    loader: 'babel'
+                },
+                {
+                    test: /\.jsx$/,
+                    include: paths,
+                    loader: 'babel'
+                }
+            ]
+        }
+    }
+}
+
+exports.minify = function() {
+    /* eslint-disable camelcase  */
+    /* Disabled due to drop_console key throwing error. */
+    return {
+        plugins: [
+            new Webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false,
+                    drop_console: true
+                },
+                mangle: {
+                    except: ['webpackJsonp']
+                }
+            })
+        ]
+    };
+    /* eslint-enable */
+}
+
+exports.setFreeVariable = function(key, value) {
+    const env = {};
+
+    env[key] = JSON.stringify(value);
+
+    return {
+        plugins: [
+            new Webpack.DefinePlugin(env)
+        ]
+    };
+}
+
+exports.extractBundle = function(options) {
+    const entry = {};
+
+    entry[options.name] = options.entries;
+
+    return {
+        // Define entry point for splitting
+        entry: entry,
+        plugins: [
+            // Extract bundle and manifest files. Manifest is needed for reliable caching.
+            new Webpack.optimize.CommonsChunkPlugin({
+                names: [options.name, 'manifest']
+            })
+        ]
+    };
+}
+
+exports.trackBundles = function(opts) {
+    return {
+        plugins: [
+            new BundleTracker(opts)
+        ]
+    }
+}
+
+exports.devServer = function(opts) {
+    return {
+        devServer: {
+            publicPath: opts.publicPath,
+            // Enable history API fallback so HTML5 History API based
+            // routing works. This is a good default that will come
+            // in handy in more complicated setups.
+            historyApiFallback: true,
+            // Unlike the cli flag, this doesn't set
+            // HotModuleReplacementPlugin!
+            hot: true,
+            inline: true,
+            // Parse host and port from env to allow customization.
+            //
+            // If you use Vagrant or Cloud9, set
+            // host: opts.host || '0.0.0.0';
+            //
+            // 0.0.0.0 is available to all network devices
+            // unlike default `localhost`.
+            host: '0.0.0.0', // Defaults to `localhost`
+            port: 3000 // Defaults to 8080
+        },
+        plugins: [
+            // Enable multi-pass compilation for enhanced performance
+            // in larger projects. Good default.
+            new Webpack.HotModuleReplacementPlugin({
+                multiStep: true
+            })
+        ]
+    }
+}
---
+++ mote/projects/skeleton/website/src/pattern-boilerplate/README.txt
@@ -0,0 +1,1 @@
+Used with `gulp genpattern --project=projectname --pattern=patternname` to quickly create boilerplate code for us to work from.

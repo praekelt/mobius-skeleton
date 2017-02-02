0daf7abe2b0c1eb2850057a0fe587be36496acc8
hedleyroos <hedleyroos@gmail.com>
2017-02-02 21:19:26 +0200

    Also run the normal collectstatic

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -6,4 +6,5 @@ chown -R ubuntu:ubuntu ${INSTALLDIR}/${REPO}
 $manage migrate --noinput --settings=project.settings >> /var/praekelt/postinstall.log 2>&1
 $manage load_photosizes --noinput --settings=project.settings >> /var/praekelt/postinstall.log 2>&1
 $manage load_layers --noinput --settings=project.settings >> /var/praekelt/postinstall.log 2>&1
+$manage collectstatic --noinput --settings=project.settings >> /var/praekelt/postinstall.log 2>&1
 $manage layers_collectstatic --noinput --settings=project.settings >> /var/praekelt/postinstall.log 2>&1

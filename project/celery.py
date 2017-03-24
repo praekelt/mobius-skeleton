c93afcb1026218b3a0206f7436914fb981a95d24
hedleyroos <hedleyroos@gmail.com>
2017-03-24 13:21:09 +0200

    Provide a dummy scheduler

--- project/celery.py
+++ project/celery.py
@@ -10,6 +10,15 @@ app = Celery("project")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
 
+
+class DummyScheduler(Scheduler):
+    """A scheduler that does nothing."""
+
+    def __init__(self, *args, **kwargs):
+        super(DummyScheduler, self).__init__(*args, **kwargs)
+        self.schedule = {}
+
+
 @app.task(bind=True)
 def debug_task(self):
     print("Request: {0!r}".format(self.request))

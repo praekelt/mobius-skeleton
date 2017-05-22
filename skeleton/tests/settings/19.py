4a5d72eaf0e87c6db0fef5804019d16148cfee8f
hedleyroos <hedleyroos@gmail.com>
2017-05-22 16:57:02 +0200

    Use newer Django settings for database

--- skeleton/tests/settings/19.py
+++ skeleton/tests/settings/19.py
@@ -110,7 +110,7 @@ WSGI_APPLICATION = "project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": "test",
         "USER": "postgres",
         "PASSWORD": "",

490e6023a2dda2ae02296cf990557027f0aa93ca
hedleyroos <hedleyroos@gmail.com>
2017-11-27 15:58:47 +0200

    Set APP_DIRS to match normal Django generate value

--- project/settings_mobius.py
+++ project/settings_mobius.py
@@ -75,7 +75,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [],
-        "APP_DIRS": False,
+        "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
                 "django.contrib.auth.context_processors.auth",

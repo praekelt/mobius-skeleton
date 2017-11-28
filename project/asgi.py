e56208caf4f264c0be80275afc82d2f91c53668d
cblignaut <cilliers@praekeltconsulting.com>
2017-11-28 10:02:51 +0200

    Change asgi.py settings order

--- project/asgi.py
+++ project/asgi.py
@@ -1,6 +1,5 @@
 import os
+os.environ["DJANGO_SETTINGS_MODULE"] = "project.settings"
+
 from channels.asgi import get_channel_layer
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
-
 channel_layer = get_channel_layer()
